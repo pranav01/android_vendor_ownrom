e2c7d326be0f842110f85d5aa37ef1ee13b244a3
Akhil Narang <akhilnarang.1999@gmail.com>
2015-05-29 18:58:04 +0530

    Fix major derp :v
    Change-Id: Ib579f36dddc45290a61dacdaa521fecb6abbe7c5
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- tools/sync-own.sh
+++ tools/sync-own.sh
@@ -1,8 +1,11 @@
 #! /bin/bash
+if [ -e ".repo" ]
+then
+. build/envsetup.sh
 . $ANDROID_BUILD_TOP/vendor/ownrom/tools/colors
 echo -e "$txtbld $cya Syncing OwnROM repositories......"
 $normal
-for repository in $(cat . vendor/ownrom/tools/ownrom-repos)
+for repository in $(cat . $ANDROID_BUILD_TOP/vendor/ownrom/tools/ownrom-repos)
 do
 echo -e "$red Syncing $repository"
 cd $ANDROID_BUILD_TOP
@@ -10,7 +13,10 @@ echo -e "$blu  `repo sync $repository | grep -i 'fetching'`"
 echo -e "$grn Synced $repository"
 done
 $normal
-
+else
+echo -e "Please make sure you are running this is the root of your build directory"
+exit;
+fi
 
 
 
